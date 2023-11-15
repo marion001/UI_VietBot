247400512cbf1f3dbc59b2e7233a0ba47eb75c33
Marion001 <27297851+marion001@users.noreply.github.com>
2023-11-15 15:51:52 +0700

    Update Get_Final_Url_ZingMp3.php

--- include_php/Ajax/Get_Final_Url_ZingMp3.php
+++ include_php/Ajax/Get_Final_Url_ZingMp3.php
@@ -1,5 +1,6 @@
 <?php
-
+include "../../Configuration.php";
+include("../../assets/lib_php/Net/SSH2.php");
 function getFinalUrl($url) {
     $ch = curl_init($url);
     // Thiết lập cURL để trả về dữ liệu thay vì hiển thị nó
@@ -26,12 +27,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['url'])) {
     // Kiểm tra xem chuỗi đầu vào có chứa 'mp3/' hay không
     if (strpos($url, 'mp3/') !== false) {
         echo json_encode(['finalUrl' => $url]);
-    } else {
+    }
+	elseif (strpos($url, "https://www.youtube.com/watch?v=") !== false) {
+		
+		//echo $url;
+		$command = "python3 $DuognDanUI_HTML/include_php/Ajax/Youtube_MP3_Link_Play.py " . escapeshellarg($url);
+		$connection = ssh2_connect($serverIP, $SSH_Port);
+		if (!$connection) {die($E_rror_HOST);}
+		if (!ssh2_auth_password($connection, $SSH_TaiKhoan, $SSH_MatKhau)) {die($E_rror);}
+		$stream = ssh2_exec($connection, $command);
+		stream_set_blocking($stream, true);
+		$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+		$output = stream_get_contents($stream_out);
+		// Trim trailing newline characters
+		$output = rtrim($output, "\n");
+		echo json_encode(['finalUrl' => $output]);
+		
+	}
+	
+	else {
         // Tiếp tục thực hiện yêu cầu cURL chỉ khi không có 'mp3/' trong đường dẫn
         $finalUrl = getFinalUrl($url);
         echo json_encode(['finalUrl' => $finalUrl]);
     }
-} else {
+} 
+else {
     echo json_encode(['error' => 'Invalid request']);
 }
 
