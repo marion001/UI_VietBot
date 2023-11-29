e74233256862e5a569821c05496b4330994bccca
Marion001 <27297851+marion001@users.noreply.github.com>
2023-11-29 18:11:05 +0700

    Update ConfigSetting.php

--- include_php/ConfigSetting.php
+++ include_php/ConfigSetting.php
@@ -8,7 +8,7 @@ include("../assets/lib_php/Net/SSH2.php");
 <?php	
 if (isset($Web_UI_Login) && $Web_UI_Login === true) {
 	if (!isset($_SESSION['root_id'])) {
-		echo "<html><head><link rel='stylesheet' href='../assets/css/loading.css'><link rel='stylesheet' href='../assets/css/bootstrap.css'></head><body><br/><center><h1>Có Vẻ Như Bạn Chưa Đăng Nhập!<br/><br>
+		echo "<!DOCTYPE html><head><link rel='stylesheet' href='../assets/css/loading.css'><link rel='stylesheet' href='../assets/css/bootstrap.css'></head><body><br/><center><h1>Có Vẻ Như Bạn Chưa Đăng Nhập!<br/><br>
 		- Nếu Bạn Đã Đăng Nhập, Hãy Nhấn Vào Nút Dưới<br/><br/><a href='$PHP_SELF'><button type='button' class='btn btn-danger'>Tải Lại</button></a></h1>
 		</center></body></html>";
 		exit();
