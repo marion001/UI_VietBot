e7774a997ecaa6cd79b8e73da6dc5c626782a5bc
Marion001 <27297851+marion001@users.noreply.github.com>
2023-11-29 17:57:48 +0700

    Update LogServiceCMD.php

--- include_php/LogServiceCMD.php
+++ include_php/LogServiceCMD.php
@@ -3,7 +3,6 @@
 // Facebook: https://www.facebook.com/TWFyaW9uMDAx
 include "../Configuration.php";
 ?>
-<!DOCTYPE html>
 <html><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
