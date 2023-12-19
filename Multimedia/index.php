45cac56f3b0e43b19382172dd8c82b5cf0d0835e
Marion001 <27297851+marion001@users.noreply.github.com>
2023-12-19 22:57:26 +0700

    Update index.php

--- Multimedia/index.php
+++ Multimedia/index.php
@@ -108,6 +108,32 @@ Facebook: https://www.facebook.com/TWFyaW9uMDAx
 		*/
 	
     </style>
+	    <style>
+        /* Your CSS styles here */
+        .popup-container {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 9999;
+        }
+
+        .popup-container.show {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        #popupContent {
+            background-color: white;
+            padding: 20px;
+            border: 1px solid gray;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 
 <body>
