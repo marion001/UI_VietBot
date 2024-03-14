91218afddc268e4d7d1a29679b5f9d767f2e4ab7
Marion001 <27297851+marion001@users.noreply.github.com>
2024-03-14 19:44:54 +0700

    Add files via upload

---
+++ include_php/Ajax/Mic_Scan.py
@@ -0,0 +1,39 @@
+import os
+import json
+import pyaudio
+
+# Thiết lập biến môi trường để tắt thông báo của ALSA
+os.environ['PYAUDIOALSADEBUG'] = '0'
+
+def list_audio_input_devices():
+    p = pyaudio.PyAudio()
+    num_devices = p.get_device_count()
+    input_devices = []
+
+    for idx in range(num_devices):
+        device_info = p.get_device_info_by_index(idx)
+        if device_info["maxInputChannels"] > 0:
+            input_devices.append({
+                "index": idx,
+                "name": device_info["name"],
+                "hostApi": device_info["hostApi"],
+                "hostApiId": p.get_host_api_info_by_index(device_info["hostApi"])["index"],
+                "maxInputChannels": device_info["maxInputChannels"],
+                "maxOutputChannels": device_info["maxOutputChannels"],
+                "defaultLowInputLatency": device_info["defaultLowInputLatency"],
+                "defaultLowOutputLatency": device_info["defaultLowOutputLatency"],
+                "defaultHighInputLatency": device_info["defaultHighInputLatency"],
+                "defaultHighOutputLatency": device_info["defaultHighOutputLatency"],
+                "defaultSampleRate": device_info["defaultSampleRate"],
+                "structVersion": device_info["structVersion"],
+                "hostApiName": p.get_host_api_info_by_index(device_info["hostApi"])["name"],
+                "deviceCount": p.get_device_count(),
+                #"deviceInfo": p.get_device_info_by_index(idx)
+            })
+
+    p.terminate()
+    return input_devices
+
+if __name__ == "__main__":
+    devices = list_audio_input_devices()
+    print(json.dumps(devices, indent=4))
