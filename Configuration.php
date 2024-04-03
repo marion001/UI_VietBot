aec990b95606b2cf5ba579dfe3ce99c5b4d44abc
Marion001 <27297851+marion001@users.noreply.github.com>
2024-04-03 21:32:29 +0700

    Update Configuration.php

--- Configuration.php
+++ Configuration.php
@@ -182,6 +182,9 @@ $Port_Vietbot = $dataVTGET->smart_config->web_interface->port;
 
 $apiKeyWeather = $Data_Json_Skill->weather->openweathermap_key;
 
+//Get Key Gemini
+$key_Gemini = $Data_Json_Skill->google_ai->api_key;
+
 $sync_media_player_checkbox = $Data_Json_Skill->ui_media_player->sync_media_player;
 $sync_media_player_sync_delay = $Data_Json_Skill->ui_media_player->sync_delay;
 //$sync_music_stream = $Data_Json_Skill->ui_media_player->music_stream;
